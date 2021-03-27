7e82fa94d88116e809ededd6ffa5a5255a4c9e52
zackrcarson <69831285+zackrcarson@users.noreply.github.com>
2021-03-27 14:32:39 -0600

    Added more tilemap details, animated NPC idling, animated players and enemies idling during battle, and more detailed attack animations, animated fish spots and waves in the water
    Added more tilemap details, animated NPC idling, animated players and enemies idling during battle, and more detailed attack animations, animated fish spots and waves in the water

---
+++ Assets/Scripts/Dialogue/RandomAnimationFrame.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class RandomAnimationFrame : MonoBehaviour
+{
+    private void Start()
+    {
+        Animator anim = GetComponent<Animator>();
+        AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo(0);//could replace 0 by any other animation layer index
+        anim.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
+    }
+
+}
