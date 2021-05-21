0a6b064589b65c5a80cccab59809c60123edf608
zackrcarson <69831285+zackrcarson@users.noreply.github.com>
2021-05-20 18:45:23 -0600

    Added a lot of signs to the countryside and Library with directions, notes, etc. Also added an intro sign near where the player starts to re-iterate all of the hints.
    Added a lot of signs to the countryside and Library with directions, notes, etc. Also added an intro sign near where the player starts to re-iterate all of the hints.

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -8,9 +8,41 @@ public class Dialogue : MonoBehaviour
     [SerializeField] string questToMark = null;
     [SerializeField] bool markQuestComplete = false;
     [SerializeField] string addNewQuest = "";
+    [SerializeField] int autoFillUnicode = 0;
 
     // State variables
     bool canActivate = false;
+
+    private void Start()
+    {
+        if (autoFillUnicode != 0)
+        {
+            dialogueLines = GetUnicodeDialogueLines(autoFillUnicode);
+        }
+    }
+
+    private string[] GetUnicodeDialogueLines(int number)
+    {
+        // UP : \u2191, DOWN:  \u2193, RIGHT: \u2192, LEFT: \u2190
+        string[] newLines = new string[2];
+        if (number == 1)
+        {
+            newLines[0] = " Caves: \u2191 \n Fountain: \u2192 \n Southern Fillory: \u2193";
+            newLines[1] = " Northern Fillory: \u2191 \n Shop: \u2192";
+        }
+        else if (number == 2)
+        {
+            newLines[0] = " Caves: \u2191 \n Fountain: \u2192 \n Southern Fillory: \u2193";
+            newLines[1] = " Northern Fillory: \u2191 \n Shop: \u2190";
+        }
+        else if (number == 3)
+        {
+            newLines[0] = " Caves: \u2191 \n Fountain: \u2192 \n Southern Fillory: \u2193";
+            newLines[1] = " Northern Fillory: \u2191 \n Shop: \u2192";
+        }
+
+        return newLines;
+    }
 
     private void Update()
     {
